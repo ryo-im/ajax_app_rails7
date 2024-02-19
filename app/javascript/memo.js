e8f5613dbe2e56f442a3d0fd1a5793a2b7c796c2
ryo-im <ir.nkym6916@gmail.com>
2024-02-19 11:47:27 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,9 +1,9 @@
-function post (){
+function post(){
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const fromData = new FormData(form);
-    const XHR = new XMLHttpRequest();
+    const formData = new FormData(form);
+    const XHR =new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
